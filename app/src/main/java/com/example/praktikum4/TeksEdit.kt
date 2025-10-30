1b3e8b1171b6078f311db04ab2f5bb5444701943
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-10-30 15:09:57 +0700

    menambahkan variable" untuk mengingat nilai masukan dari keyboard

---
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -0,0 +1,14 @@
+package com.example.praktikum4
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier
+){
+    //variable" untuk mengingat nilai masukan dari keyboard
+    var textNama by remember {mutableMapOf("")}
+    var textAlamat by remember {mutableMapOf("")}
+    var textJK by remember {mutableMapOf("")}
+}
